2e07ea17b1477434f6a3bf6777bd5163c713d435
zhaopeng <zhaopeng@qujiali.com>
2018-08-11 10:01:33 +0800

    热部署和log4j日志管理

--- src/main/java/com/zyc/controller/DemoUserController.java
+++ src/main/java/com/zyc/controller/DemoUserController.java
@@ -2,6 +2,8 @@ package com.zyc.controller;
 
 import com.zyc.entity.DemoUser;
 import com.zyc.resultTemplate.ResultGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class DemoUserController {
+    private static final Logger log = LoggerFactory.getLogger(DemoUserController.class);
 
     @GetMapping("/getUser")
     public Object getUser() {
-        return ResultGenerator.successResult(new DemoUser(1L, "张三", 25));
+        DemoUser user = new DemoUser(1L, "张三", 245);
+        log.info(user.toString());
+        log.error(user.toString());
+        return ResultGenerator.successResult(user);
     }
 }
