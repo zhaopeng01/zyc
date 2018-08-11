77fe8d5cf09c0343897921227f762937827a450f
zhaopeng <zhaopeng@qujiali.com>
2018-08-11 10:56:08 +0800

    swagger 整合

--- src/main/java/com/zyc/controller/DemoUserController.java
+++ src/main/java/com/zyc/controller/DemoUserController.java
@@ -2,6 +2,8 @@ package com.zyc.controller;
 
 import com.zyc.entity.DemoUser;
 import com.zyc.resultTemplate.ResultGenerator;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Api(value = "demoUser模块", description = "web-demoUser模块")
 public class DemoUserController {
     private static final Logger log = LoggerFactory.getLogger(DemoUserController.class);
 
+    @ApiOperation(value="获取demoUse1r")
     @GetMapping("/getUser")
     public Object getUser() {
         DemoUser user = new DemoUser(1L, "张三", 245);
